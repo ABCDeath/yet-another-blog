a409d660345868b94e6d45e7f811b076f180eed6
ABCDeath <cob19721@yandex.ru>
2019-02-08 16:30:56 +0300

    FeedView на основе дженерика ListView с примесью для необходимости логина. При создании пользователя автоматически создается профиль.

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .models import Post, Profile
+
+
+class FeedView(LoginRequiredMixin, generic.ListView):
+    model = Post
+
+    login_url = '/accounts/login/'
+
+    template_name = 'blog_app/feed.html'
+    context_object_name = 'posts_feed'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (Post.objects.prefetch_related('author')
+                .filter(author__in=self.request.user.profile.subscription.all())
+                .order_by('-pub_date'))
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+

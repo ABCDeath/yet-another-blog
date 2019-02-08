ef92e501a4239fe3341843220204917e10119a69
ABCDeath <cob19721@yandex.ru>
2019-02-08 14:09:05 +0300

    Модель профиля пользователя и модель поста.

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscription = models.ManyToManyField('self')
+
+class Post(models.Model):
+    caption = models.CharField(max_length=128)
+    content_text = models.CharField(max_length=2048)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+

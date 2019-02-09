1b10e702d98976d33063291b4cd0719814163bb2
ABCDeath <cob19721@yandex.ru>
2019-02-09 20:59:41 +0300

    Фикс подписки в модели профиля: она не должна быть симметричной.

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    subscription = models.ManyToManyField('self')
+    subscription = models.ManyToManyField('self', symmetrical=False)
 
     def __str__(self):
         return f'{self.user.username} ({self.user.get_full_name()})'

89b349b178361731009b9371b9ba7fb44da199de
ABCDeath <cob19721@yandex.ru>
2019-02-11 14:03:46 +0300

    Тест подписки на самого себя.

--- blog/blog_app/tests.py
+++ blog/blog_app/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from .models import Profile, Post
@@ -6,14 +7,22 @@ from .models import Profile, Post
 
 class ProfileModelTest(TestCase):
     def test_profile_auto_create(self):
-        username = 'test_user'
+        username = 'user'
         user = User.objects.create_user(username, '', 'testpassword')
 
         self.assertQuerysetEqual(
-            Profile.objects.all(), ['<Profile: test_user ()>'])
+            Profile.objects.all(), [f'<Profile: {username} ()>'])
 
         self.assertEqual(
             user, Profile.objects.get(user__username=username).user)
 
         self.assertEqual(
             user.profile, Profile.objects.get(user__username=username))
+
+    def test_follow_self(self):
+        username = 'user'
+        user = User.objects.create_user(username, '', 'testpassword')
+
+        with self.assertRaises(ValidationError):
+            user.profile.following.add(user.profile)
+            self.assertQuerysetEqual(user.profile.following.all(), [])

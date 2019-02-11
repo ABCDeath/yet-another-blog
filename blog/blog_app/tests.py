852ed5740b49724b160587704b941cc8821e9bd6
ABCDeath <cob19721@yandex.ru>
2019-02-11 14:34:36 +0300

    Тест удаления отметок о прочитанных постах при отписке от пользователя.

--- blog/blog_app/tests.py
+++ blog/blog_app/tests.py
@@ -26,3 +26,28 @@ class ProfileModelTest(TestCase):
         with self.assertRaises(ValidationError):
             user.profile.following.add(user.profile)
             self.assertQuerysetEqual(user.profile.following.all(), [])
+
+    def test_unfollow_read_posts_remove(self):
+        username = 'user'
+        user = User.objects.create_user(username, '', 'testpassword')
+
+        following_uname = 'following'
+        following = User.objects.create_user(
+            following_uname, '', 'testpassword')
+
+        user.profile.following.add(following.profile)
+
+        p = Post(caption='c', content_text='t', author=following.profile)
+        p.save()
+
+        user.profile.posts_read.add(p)
+
+        self.assertEqual(p, user.profile.posts_read.all()[0])
+
+        user.profile.following.remove(following.profile)
+
+        self.assertQuerysetEqual(user.profile.posts_read.all(), [])
+
+        user.profile.following.add(following.profile)
+
+        self.assertQuerysetEqual(user.profile.posts_read.all(), [])

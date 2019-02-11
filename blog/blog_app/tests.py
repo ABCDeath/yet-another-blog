d95f5c1b9fa5b9f336dea98327db598d6afd21a3
ABCDeath <cob19721@yandex.ru>
2019-02-11 13:50:06 +0300

    Тест автоматического создания и привязки Profile при создании User.

--- blog/blog_app/tests.py
+++ blog/blog_app/tests.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Profile, Post
+
+
+class ProfileModelTest(TestCase):
+    def test_profile_auto_create(self):
+        username = 'test_user'
+        user = User.objects.create_user(username, '', 'testpassword')
+
+        self.assertQuerysetEqual(
+            Profile.objects.all(), ['<Profile: test_user ()>'])
+
+        self.assertEqual(
+            user, Profile.objects.get(user__username=username).user)
+
+        self.assertEqual(
+            user.profile, Profile.objects.get(user__username=username))
